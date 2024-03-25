e0b3133683a346d0c85899c6d3463540b0884677
Charles <samannaphala@gmail.com>
2024-03-25 06:57:14 +0200

    adds compression to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
+import compression from 'vite-plugin-compression2'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    compression({
+      algorithm: 'gzip', exclude: [/\.(br)$ /, /\.(gz)$/]
+    }),
+    compression({
+      algorithm: 'brotliCompress', exclude: [/\.(br)$ /, /\.(gz)$/],
+     }),
+  ],
 })
